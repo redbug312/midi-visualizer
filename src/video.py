264cb25c08ff4fcf0240ab302fc7310484b75a33
redbug312 <redbug312@gmail.com>
2022-08-11 18:40:51 +0800

    Replace one of the midi files
    Also rename the file names in kebab-case

--- src/video.py
+++ src/video.py
@@ -217,6 +217,6 @@ if __name__ == '__main__':
     # from gi.repository import Gst, Gtk, GLib
     from parser import Midi
 
-    sheet = Midi('midi/The Positive and Negative.mid')
+    sheet = Midi('midi/the-positive-and-negative.mid')
     clip = midi_videoclip(sheet)
-    clip.write_videofile('/tmp/test.webm', fps=60, audio=False, threads=4)
+    clip.write_videofile('/tmp/test.webm', fps=30, audio=False, threads=4)
