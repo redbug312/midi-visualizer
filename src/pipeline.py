b961d94854948483a71abd36cab85050e4237fc2
redbug312 <redbug312@gmail.com>
2022-08-13 23:43:51 +0800

    Refactor Gstreamer pipeline code

--- src/pipeline.py
+++ src/pipeline.py
@@ -4,44 +4,36 @@ gi.require_version('Gst', '1.0')
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gst, Gtk
 
-# Gst utilizing functions
 
 class Player:
     def __init__(self):
-        frontend_elements = ['webmsrc', 'matroskademux', 'webmqueue',
-                             'midisrc', 'midiparse', 'fluiddec']
-        backend_elements = ['vp8dec', 'videoconvert', 'gtksink',
-                            'autoaudiosink']
-        frontend = make_frontend_pipeline('frontend', frontend_elements)
-        play_backend = make_play_backend_pipeline('backend', backend_elements)
-        frontend.get_by_name('fluiddec').set_property('soundfont', 'soundfont/touhou.sf2')
-
         self.pipeline = Gst.Pipeline.new('player')
-        self.pipeline.add(frontend)
-        self.pipeline.add(play_backend)
-        frontend.link_pads('video_src', play_backend, 'video_sink')
-        frontend.link_pads('audio_src', play_backend, 'audio_sink')
+        self.elements = dict()
+
+        self.load_pipe, self.elements['load'] = make_load_pipeline()
+        self.play_pipe, self.elements['play'] = make_play_pipeline()
+        self.save_pipe, self.elements['save'] = make_save_pipeline()
+
+        self.pipeline.add(self.load_pipe)
+        self.pipeline.add(self.play_pipe)
+        self.load_pipe.link_pads('video_sink', self.play_pipe, 'video_src')
+        self.load_pipe.link_pads('audio_sink', self.play_pipe, 'audio_src')
 
     def load(self, webm, midi):
-        webmsrc = self.pipeline.get_by_name('webmsrc')
-        midisrc = self.pipeline.get_by_name('midisrc')
+        webmsrc = self.elements['load'][0]
+        midisrc = self.elements['load'][3]
         webmsrc.set_property('location', webm)
         midisrc.set_property('location', midi)
 
     def save(self, file):
-        backend_elements = ['webmmux', 'filesink',
-                            'audioconvert', 'vorbisenc', 'midiqueue']
-        frontend     = self.pipeline.get_by_name('frontend')
-        play_backend = self.pipeline.get_by_name('backend').ref()  # Avoid making the same subpipeline
-        save_backend = make_save_backend_pipeline('backend', backend_elements)
-        save_backend.get_by_name('filesink').set_property('location', file)
-
+        filesink = self.elements['save'][1]
+        filesink.set_property('location', file)
 
         self.pipeline.set_state(Gst.State.NULL)
-        self.pipeline.remove(play_backend)
-        self.pipeline.add(save_backend)
-        frontend.link_pads('video_src', save_backend, 'video_sink')
-        frontend.link_pads('audio_src', save_backend, 'audio_sink')
+        self.pipeline.remove(self.play_pipe)
+        self.pipeline.add(self.save_pipe)
+        self.load_pipe.link_pads('video_sink', self.save_pipe, 'video_src')
+        self.load_pipe.link_pads('audio_sink', self.save_pipe, 'audio_src')
 
         self.pipeline.set_state(Gst.State.PLAYING)
         bus = self.pipeline.get_bus()
@@ -52,82 +44,75 @@ class Player:
                 Gtk.main_iteration()
 
         self.pipeline.set_state(Gst.State.NULL)
-        self.pipeline.remove(save_backend)
-        self.pipeline.add(play_backend)
-        frontend.link_pads('video_src', play_backend, 'video_sink')
-        frontend.link_pads('audio_src', play_backend, 'audio_sink')
+        self.pipeline.remove(self.save_pipe)
+        self.pipeline.add(self.play_pipe)
+        self.load_pipe.link_pads('video_sink', self.play_pipe, 'video_src')
+        self.load_pipe.link_pads('audio_sink', self.play_pipe, 'audio_src')
 
     def widget(self):
-        return self.pipeline.get_by_name('gtksink').props.widget
+        gtksink = self.elements['play'][2]
+        return gtksink.props.widget
 
     def draw_pipeline(self):
         Gst.debug_bin_to_dot_file(self.pipeline, Gst.DebugGraphDetails.ALL, 'pipeline')
-        print('Ok')
 
 
-def make_frontend_pipeline(pipeline_name, element_names):
-    links = [('filesrc', 1), ('matroskademux', None), ('queue', None),
-             ('filesrc', 4), ('midiparse', 5), ('fluiddec', None)]
-    pipeline = Gst.Pipeline.new(pipeline_name)
-    pipeline_get_by_index = lambda i: pipeline.get_by_name(element_names[i])
+def extend_pipe(pipeline, names):
+    elements = [Gst.ElementFactory.make(name) for name in names]
+    for element in elements:
+        pipeline.add(element)
+    for pred, succ in zip(elements, elements[1:]):
+        assert pred.link(succ), 'failed to link %s to %s' % (pred, succ)
+    return elements
 
-    for param in zip([link[0] for link in links], element_names):
-        pipeline.add(Gst.ElementFactory.make(*param))
-    for src, sink in [(i, x[1]) for i, x in enumerate(links) if x[1] is not None]:
-        pipeline_get_by_index(src).link(pipeline_get_by_index(sink))
 
-    def on_demux_pad_added(demux, pad):
+def make_load_pipeline():
+    pipeline = Gst.Pipeline.new('load')
+    elements = []
+    elements += extend_pipe(pipeline, ['filesrc', 'matroskademux'])
+    elements += extend_pipe(pipeline, ['queue'])
+    elements += extend_pipe(pipeline, ['filesrc', 'midiparse', 'fluiddec'])
+
+    def on_demux_pad_added(_, pad):
         caps = pad.query_caps(None)
-        structure_name = caps.to_string()
-        if structure_name.startswith('video'):
-            pad.link(pipeline_get_by_index(2).get_static_pad('sink'))
+        if caps.to_string().startswith('video'):
+            pad.link(elements[2].get_static_pad('sink'))
 
-    pipeline_get_by_index(1).connect('pad-added', on_demux_pad_added)
-    pipeline.add_pad(Gst.GhostPad.new('video_src', pipeline_get_by_index(2).get_static_pad('src')))
-    pipeline.add_pad(Gst.GhostPad.new('audio_src', pipeline_get_by_index(5).get_static_pad('src')))
-    return pipeline
+    elements[1].connect('pad_added', on_demux_pad_added)
+    elements[5].set_property('soundfont', 'soundfont/touhou.sf2')
 
-
-def make_play_backend_pipeline(pipeline_name, element_names):
-    # Gst.parse_launch("""
-    #     filesrc location=..webm ! matroskademux ! queue ! vp8dec ! videoconvert ! gtksink
-    #     filesrc location=..mid ! midiparse ! fluiddec soundfont=..sf2 ! autoaudiosink
-    # """)
-    links = [('vp8dec', 1), ('videoconvert', 2), ('gtksink', None),
-             ('autoaudiosink', None)]
-    pipeline = Gst.Pipeline.new(pipeline_name)
-    pipeline_get_by_index = lambda i: pipeline.get_by_name(element_names[i])
-
-    for param in zip([link[0] for link in links], element_names):
-        pipeline.add(Gst.ElementFactory.make(*param))
-    for src, sink in [(i, x[1]) for i, x in enumerate(links) if x[1] is not None]:
-        pipeline_get_by_index(src).link(pipeline_get_by_index(sink))
-
-    pipeline.add_pad(Gst.GhostPad.new('video_sink', pipeline_get_by_index(0).get_static_pad('sink')))
-    pipeline.add_pad(Gst.GhostPad.new('audio_sink', pipeline_get_by_index(3).get_static_pad('sink')))
-    return pipeline
+    video_sink = Gst.GhostPad.new('video_sink', elements[2].get_static_pad('src'))
+    audio_sink = Gst.GhostPad.new('audio_sink', elements[5].get_static_pad('src'))
+    pipeline.add_pad(video_sink)
+    pipeline.add_pad(audio_sink)
+    return pipeline, elements
 
 
-def make_save_backend_pipeline(pipeline_name, element_names):
-    # Gst.parse_launch("""
-    #     webmmux name=mux ! filesink location="test.webm"
-    #     filesrc location=..webm ! matroskademux ! queue ! mux
-    #     filesrc location=..mid ! midiparse ! fluiddec soundfont=..sf2 !\
-    #         audioconvert ! vorbisenc ! queue ! mux
-    # """)
-    links = [('webmmux', 1), ('filesink', None),
-             ('audioconvert', 3), ('vorbisenc', 4), ('queue', 0)]
-    pipeline = Gst.Pipeline.new(pipeline_name)
-    pipeline_get_by_index = lambda i: pipeline.get_by_name(element_names[i])
+def make_play_pipeline():
+    pipeline = Gst.Pipeline.new('play')
+    elements = []
+    elements += extend_pipe(pipeline, ['vp8dec', 'videoconvert', 'gtksink'])
+    elements += extend_pipe(pipeline, ['autoaudiosink'])
 
-    for param in zip([link[0] for link in links], element_names):
-        pipeline.add(Gst.ElementFactory.make(*param))
-    for src, sink in [(i, x[1]) for i, x in enumerate(links) if x[1] is not None]:
-        pipeline_get_by_index(src).link(pipeline_get_by_index(sink))
+    video_src = Gst.GhostPad.new('video_src', elements[0].get_static_pad('sink'))
+    audio_src = Gst.GhostPad.new('audio_src', elements[3].get_static_pad('sink'))
+    pipeline.add_pad(video_src)
+    pipeline.add_pad(audio_src)
+    return pipeline, elements
 
-    pipeline.add_pad(Gst.GhostPad.new('video_sink', pipeline_get_by_index(0).get_request_pad('video_0')))
-    pipeline.add_pad(Gst.GhostPad.new('audio_sink', pipeline_get_by_index(2).get_static_pad('sink')))
-    return pipeline
+
+def make_save_pipeline():
+    pipeline = Gst.Pipeline.new('save')
+    elements = []
+    elements += extend_pipe(pipeline, ['webmmux', 'filesink'])
+    elements += extend_pipe(pipeline, ['audioconvert', 'vorbisenc', 'queue'])
+
+    elements[4].link(elements[0])  # use queue before a mux
+    video_src = Gst.GhostPad.new('video_src', elements[0].get_request_pad('video_0'))
+    audio_src = Gst.GhostPad.new('audio_src', elements[2].get_static_pad('sink'))
+    pipeline.add_pad(video_src)
+    pipeline.add_pad(audio_src)
+    return pipeline, elements
 
 
 if __name__ == '__main__':
